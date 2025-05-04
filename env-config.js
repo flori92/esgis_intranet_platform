773d80462f3747b85003b526715586819d567de5
flori92 <56716261+flori92@users.noreply.github.com>
2025-05-04 08:44:04 +0000

    Deploying to gh-pages from @ flori92/esgis_intranet_platform@4e9e145485dd80b6cbe66785d88e451c1dd56124 🚀

---
+++ env-config.js
@@ -0,0 +1,6 @@
+// Configuration des variables d'environnement pour Supabase
+// Généré automatiquement lors du déploiement le Sun May  4 08:44:02 UTC 2025
+window.ENV = {
+  SUPABASE_URL: "https://epnhnjkbxgciojevrwfq.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwbmhuamtieGdjaW9qZXZyd2ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMDY5MDYsImV4cCI6MjA2MTc4MjkwNn0.VeqmGA56qySH_f4rwk6bnsvPS6173BtoRA0iCjXnogM"
+};
