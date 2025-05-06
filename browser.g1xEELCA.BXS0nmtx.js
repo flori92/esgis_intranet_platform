07b128adb6ac1bf53e83c0c53a5296c882beb90e
flori92 <56716261+flori92@users.noreply.github.com>
2025-05-06 11:58:54 +0000

    Deploying to gh-pages from @ flori92/esgis_intranet_platform@698cdfcaff8de6e938f543e6454073c7879dbcb3 🚀

--- browser.g1xEELCA.BXS0nmtx.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./index.DIHuaNHC.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),n=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{n as b};
